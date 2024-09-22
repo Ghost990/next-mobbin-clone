9f212f7503591691c27948e61cf966fbd64ec233
Zsolt Nagy <ghost990@gmail.com>
2024-09-22 08:13:00 +0200

    fix

--- components/carousels/carousel-card.tsx
+++ components/carousels/carousel-card.tsx
@@ -36,7 +36,7 @@ import { useState } from "react";
 import PhoneScreen from "../../public/images/phone-screen.webp";
 import { mock } from "node:test"; 
 
-export function CarouselCard({ title, images }: { title?: string; images?: object[] }) {
+export function CarouselCard({ title, images }: { title?: string; images?: any[] }) {
   const [menuOpen, setMenuOpen] = useState(false);
   const [api, setApi] = React.useState<CarouselApi>(); 
   const [current, setCurrent] = React.useState(0);
